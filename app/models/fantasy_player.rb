7443fada830cea0f8a5b60c9b1c19120569b4c24
Austin Pierce <AustinPierce@gmail.com>
2018-08-08 20:12:30 -0400

    Fantasy Player insert

--- app/models/fantasy_player.rb
+++ app/models/fantasy_player.rb
@@ -2,5 +2,12 @@ class FantasyPlayer < ApplicationRecord
   
   belongs_to :player
   belongs_to :fantasy_team
+  
+  def self.InsertPlayers
+    players = Player.all.ids
+    
+    players.sort!
+    players.each { |x| FantasyPlayer.create(player_id: "#{x}") }
+  end
  
 end
