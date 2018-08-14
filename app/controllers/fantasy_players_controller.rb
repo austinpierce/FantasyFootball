bf719606791c1b11ba939f5e669c306782c9d2b2
Austin Pierce <AustinPierce@gmail.com>
2018-08-13 21:28:46 -0400

    Edit and update fantasy player

--- app/controllers/fantasy_players_controller.rb
+++ app/controllers/fantasy_players_controller.rb
@@ -1,7 +1,29 @@
 class FantasyPlayersController < ApplicationController
+  #before_action :find_fantasy_player
   
   def edit
     @fantasy_player = Player.joins(:fantasy_player).find(params[:id])
+    @teams = FantasyTeam.all
   end
   
+  def update
+    @fantasy_player = FantasyPlayer.find(params[:id])
+    if @fantasy_player.update(fantasy_player_params)
+      flash[:notice] = "Updated"
+      redirect_to players_path
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+ 
+    def find_fantasy_player
+      @fantasy_player = Player.joins(:fantasy_player).find(params[:id])
+    end
+    
+    def fantasy_player_params
+      params.require(:fantasy_player).permit(:fantasy_team_id, :watch, :keeper, :auction_spend_price, :auction_sold_price)
+    end
+    
 end
