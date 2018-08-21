86b7154330050be5be579f57a566bd6c9aef950e
Austin Pierce <AustinPierce@gmail.com>
2018-08-20 22:39:23 -0400

    Projections for all players

--- app/models/fantasy_projection.rb
+++ app/models/fantasy_projection.rb
@@ -29,7 +29,7 @@ class FantasyProjection < ApplicationRecord
 
   end
   
-def self.RB_InitialProjections
+  def self.RB_InitialProjections
     rbs = HTTParty.get("https://www.fantasyfootballnerd.com/service/draft-projections/json/test/RB/")
 
     playerSize = rbs["DraftProjections"].size
@@ -50,6 +50,107 @@ def self.RB_InitialProjections
       fantasyPoints = rb["fantasyPoints"]
       
       FantasyProjection.create(player_id: "#{playerId}", position: "RB", rush_att: "#{rushAtt}", rush_yards: "#{rushYards}", rush_td: "#{rushTD}", fumbles: "#{fumbles}", rec: "#{rec}", rec_yards: "#{recYards}", rec_td: "#{recTD}", fantasy_points: "#{fantasyPoints}")
+      playerSize -= 1
+      arrayPosition += 1
+    end
+
+  end
+  
+  def self.WR_InitialProjections
+    wrs = HTTParty.get("https://www.fantasyfootballnerd.com/service/draft-projections/json/test/WR/")
+
+    playerSize = wrs["DraftProjections"].size
+    arrayPosition = 0
+
+    until playerSize == 0
+      
+      wr = wrs["DraftProjections"][arrayPosition]
+      
+      playerId = wr["playerId"]
+      rushAtt = wr["rushAtt"]
+      rushYards = wr["rushYards"]
+      rushTD = wr["rushTD"]
+      fumbles = wr["fumbles"]
+      rec = wr["rec"]
+      recYards = wr["recYards"]
+      recTD = wr["recTD"]
+      fantasyPoints = wr["fantasyPoints"]
+      
+      FantasyProjection.create(player_id: "#{playerId}", position: "WR", rush_att: "#{rushAtt}", rush_yards: "#{rushYards}", rush_td: "#{rushTD}", fumbles: "#{fumbles}", rec: "#{rec}", rec_yards: "#{recYards}", rec_td: "#{recTD}", fantasy_points: "#{fantasyPoints}")
+      playerSize -= 1
+      arrayPosition += 1
+    end
+
+  end
+  
+  def self.TE_InitialProjections
+    tes = HTTParty.get("https://www.fantasyfootballnerd.com/service/draft-projections/json/test/TE/")
+
+    playerSize = tes["DraftProjections"].size
+    arrayPosition = 0
+
+    until playerSize == 0
+      
+      te = tes["DraftProjections"][arrayPosition]
+      
+      playerId = te["playerId"]
+      rushAtt = te["rushAtt"]
+      rushYards = te["rushYards"]
+      rushTD = te["rushTD"]
+      fumbles = te["fumbles"]
+      rec = te["rec"]
+      recYards = te["recYards"]
+      recTD = te["recTD"]
+      fantasyPoints = te["fantasyPoints"]
+      
+      FantasyProjection.create(player_id: "#{playerId}", position: "TE", rush_att: "#{rushAtt}", rush_yards: "#{rushYards}", rush_td: "#{rushTD}", fumbles: "#{fumbles}", rec: "#{rec}", rec_yards: "#{recYards}", rec_td: "#{recTD}", fantasy_points: "#{fantasyPoints}")
+      playerSize -= 1
+      arrayPosition += 1
+    end
+
+  end
+  
+  def self.DEF_InitialProjections
+    defenses = HTTParty.get("https://www.fantasyfootballnerd.com/service/draft-projections/json/test/DEF/")
+
+    playerSize = defenses["DraftProjections"].size
+    arrayPosition = 0
+
+    until playerSize == 0
+      
+      defense = defenses["DraftProjections"][arrayPosition]
+      
+      playerId = defense["playerId"]
+      sacks = defense["sacks"]
+      interceptions = defense["interceptions"]
+      fumbleRec = defense["fumbleRec"]
+      td = defense["TD"]
+      specialTeamTD = defense["specialTeamTD"]
+      fantasyPoints = defense["fantasyPoints"]
+      
+      FantasyProjection.create(player_id: "#{playerId}", position: "DEF", sacks: "#{sacks}", interceptions: "#{interceptions}", fumble_rec: "#{fumbleRec}", td: "#{td}", special_team_td: "#{specialTeamTD}", fantasy_points: "#{fantasyPoints}")
+      playerSize -= 1
+      arrayPosition += 1
+    end
+
+  end
+  
+  def self.K_InitialProjections
+    kickers = HTTParty.get("https://www.fantasyfootballnerd.com/service/draft-projections/json/test/K/")
+
+    playerSize = kickers["DraftProjections"].size
+    arrayPosition = 0
+
+    until playerSize == 0
+      
+      kicker = kickers["DraftProjections"][arrayPosition]
+      
+      playerId = kicker["playerId"]
+      xp = kicker["xp"]
+      fg = kicker["fg"]
+      fantasyPoints = kicker["fantasyPoints"]
+      
+      FantasyProjection.create(player_id: "#{playerId}", position: "K", xp: "#{xp}", fg: "#{fg}", fantasy_points: "#{fantasyPoints}")
       playerSize -= 1
       arrayPosition += 1
     end
