dbb290e5dea089f17425539e9c59855b4f7b396a
Austin Pierce <AustinPierce@gmail.com>
2018-08-21 19:59:58 -0400

    Run model method from view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,43 @@ class WelcomeController < ApplicationController
   def index
     
   end
-   
+  
+  def hello_world
+    puts "asdjfalsdjflasjfdsl"
+  end
+  
+  def load_draft_rankings
+    # execute first
+    Player.InitialDraftRankings  
+  end
+  
+  def load_fantasy_players
+    # execute second
+    FantasyPlayer.InsertPlayers  
+  end
+  
+  def load_fantasy_projections
+    # execute third
+    FantasyProjection.QB_InitialProjections  
+    FantasyProjection.RB_InitialProjections
+    FantasyProjection.WR_InitialProjections  
+    FantasyProjection.TE_InitialProjections
+    FantasyProjection.DEF_InitialProjections  
+    FantasyProjection.K_InitialProjections
+  end
+  
+  def load_auction
+    # execute fourth
+    Auction.InitialAuction
+    Auction.UpdateProjectedPrice
+  end
+  
+  def code_red
+    # destroys everything
+    Player.destroy_all
+    FantasyPlayer.destroy_all
+    FantasyProjection.destroy_all
+    Auction.destroy_all
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   resources :fantasy_projections, only: [:show]
   root 'welcome#index'
   get 'players', to: 'players#index'
+  get '/hello_world', to: 'welcome#hello_world'
+  get '/load_draft_rankings', to: 'welcome#load_draft_rankings'
+  get '/load_fantasy_players', to: 'welcome#load_fantasy_players'
+  get '/load_fantasy_projections', to: 'welcome#load_fantasy_projections'
+  get '/load_auction', to: 'welcome#load_auction'
+  get '/code_red', to: 'welcome#code_red'
+  
   
 end
